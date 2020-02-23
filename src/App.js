0c2b540507343e5f64757b0695cca87c16c94c37
c0sta <gabrielcostamoura1@gmail.com>
2020-02-23 20:26:59 -0300

    feat: added tests w/ Enzyme in App component

--- src/App.js
+++ src/App.js
@@ -10,14 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hellow world!{' '}
-        </a>
+        <h1 className="hello">Hellow world!</h1>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hellow /i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = mount(<App />);
+  expect(wrapper.find('.hello').text()).toContain('Hellow world!');
 });
