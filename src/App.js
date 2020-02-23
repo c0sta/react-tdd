db0ba48a5232a495c159b9de41af862adf473600
c0sta <gabrielcostamoura1@gmail.com>
2020-02-23 18:33:50 -0300

    feat: created simple cypress test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hellow world!{' '}
         </a>
       </header>
     </div>
